d76ecff63556d2da652097c6e167d00e405ac7f7
CodeCreatorBot <79366049+CodeCreatorBot@users.noreply.github.com>
2021-03-07 12:26:43 +0400

    listening port altered rto 3000

--- server/backend/server.js
+++ server/backend/server.js
@@ -3,7 +3,7 @@ let MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // use when starting application locally
 let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
@@ -23,49 +23,6 @@ mongoose.connect(`${mongoUrlDocker}/${databaseName}`, mongoClientOptions).then(
     console.log(`Mongooose Connection Unsuccesful :(`)
 );
 
-/*app.post('/update-profile', function(req, res) {
-    let userObj = req.body;
-
-    MongoClient.connect(mongoUrlDocker, mongoClientOptions, function(err, client) {
-        if (err) throw err;
-
-        let db = client.db(databaseName);
-        userObj['userid'] = 1;
-
-        let myquery = { userid: 1 };
-        let newvalues = { $set: userObj };
-
-        db.collection("users").updateOne(myquery, newvalues, { upsert: true }, function(err, res) {
-            if (err) throw err;
-            client.close();
-        });
-
-    });
-    // Send response
-    res.send(userObj);
-});
-
-app.get('/get-profile', function(req, res) {
-    let response = {};
-    // Connect to the db
-    MongoClient.connect(mongoUrlDocker, mongoClientOptions, function(err, client) {
-        if (err) throw err;
-
-        let db = client.db(databaseName);
-
-        let myquery = { userid: 1 };
-
-        db.collection("users").findOne(myquery, function(err, result) {
-            if (err) throw err;
-            response = result;
-            client.close();
-
-            // Send response
-            res.send(response ? response : {});
-        });
-    });
-});
-*/
 app.listen(port, () => {
     console.log(`Server is up and runing on port ${port}`);
 })
